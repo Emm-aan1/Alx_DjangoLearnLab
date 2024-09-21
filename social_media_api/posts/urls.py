8da3cb790405930096c4f5bf33919ed0166bf7a1
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-09-21 21:11:05 +0100

    save

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostCreateView, PostRetrieveUpdateDestroyView
+from .views import PostListView, PostCreateView, PostRetrieveUpdateDestroyView, like_post, unlike_post
 
 urlpatterns = [
     path('feed/', PostListView.as_view(), name='post-feed'),
     path('posts/new/', PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/', PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/like/', like_post, name='post-like'),
+    path('posts/<int:pk>/unlike/', unlike_post, name='post-unlike'),
 ]
