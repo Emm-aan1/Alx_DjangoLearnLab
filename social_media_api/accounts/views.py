dceeec3025645cfc5f00159e4c2a3c6ade15d740
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-09-21 17:58:28 +0100

    Week 15 task

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
@@ -16,7 +16,23 @@ class RegisterView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
     def get(self, request):
         user = request.user
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+class FollowUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        try:
+            user_to_follow = CustomUser.objects.get(id=user_id)
+            if request.user != user_to_follow:
+                request.user.following.add(user_to_follow)
+                return Response({'message': f'You are now following {user_to_follow.username}'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'You cannot follow yourself'}, status=status.HTTP_400_BAD_REQUEST)
+        except CustomUser.DoesNotExist:
+            return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
