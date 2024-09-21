4168748550e9817e66287051903d5ae5fceef2df
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-09-21 21:06:04 +0100

    save

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -33,28 +33,3 @@ class Like(models.Model):
     def __str__(self):
         return f"Like by {self.user.username} on {self.post}"
 
-class Notification(models.Model):
-    recipient = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
-        on_delete=models.CASCADE, 
-        related_name='notifications'
-    )
-    actor = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
-        on_delete=models.CASCADE, 
-        related_name='actions'
-    )
-    verb = models.CharField(max_length=255)
-    target_content_type = models.ForeignKey(
-        ContentType, 
-        on_delete=models.CASCADE
-    )
-    target_object_id = models.PositiveIntegerField()
-    target = GenericForeignKey(
-        'target_content_type', 
-        'target_object_id'
-    )
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Notification for {self.recipient}: {self.verb} by {self.actor}"
