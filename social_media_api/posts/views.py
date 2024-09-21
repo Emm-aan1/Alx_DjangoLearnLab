26839fd5e94974d49581badaec5ea86462f0f606
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-09-21 21:09:58 +0100

    save

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -35,7 +35,7 @@ class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def like_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = generics.get_object_or_404(Post, pk=pk)
     like, created = Like.objects.get_or_create(user=request.user, post=post)
     
     if created:
