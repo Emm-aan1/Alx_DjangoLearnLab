ca63df9bfa27c7e8f62fe9762618e848a0134116
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-09-21 17:37:50 +0100

    Week 15 task

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,11 +1,9 @@
 from rest_framework import generics
 from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
 from rest_framework.permissions import IsAuthenticated
 
 class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
@@ -13,12 +11,10 @@ class PostListCreateView(generics.ListCreateAPIView):
 
 class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
 class CommentListCreateView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
@@ -26,5 +22,4 @@ class CommentListCreateView(generics.ListCreateAPIView):
 
 class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
