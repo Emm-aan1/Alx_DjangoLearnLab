cd2c4ec500e2fc163b40830f6cb1744b86a115cb
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-09-21 21:08:16 +0100

    save

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,9 @@
-from rest_framework import generics, permissions
-from .models import Post, Comment
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from django.shortcuts import get_object_or_404
+from .models import Post, Comment, Like
+from notifications.models import Notification
 from rest_framework.permissions import IsAuthenticated
 
 class PostListCreateView(generics.ListCreateAPIView):
@@ -7,7 +11,6 @@ class PostListCreateView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        # Assuming 'following' is a related name for a ManyToMany relationship on the user model
         following_users = user.following.all()  
         return Post.objects.filter(author__in=following_users).order_by('-created_at')
 
@@ -28,3 +31,20 @@ class CommentListCreateView(generics.ListCreateAPIView):
 class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     permission_classes = [permissions.IsAuthenticated]
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def like_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    like, created = Like.objects.get_or_create(user=request.user, post=post)
+    
+    if created:
+        Notification.objects.create(
+            recipient=post.author,
+            actor=request.user,
+            verb='liked',
+            target=post
+        )
+        return Response({'status': 'post liked'}, status=status.HTTP_201_CREATED)
+    else:
+        return Response({'status': 'post already liked'}, status=status.HTTP_200_OK)
